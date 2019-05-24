0f958d6ba4364445721b628bb28f8863733f17c5
Erica Tom <erica.tom98@yahoo.com>
2019-05-23 20:32:40 -0700

    add commentation and updated README

--- README.md
+++ README.md
@@ -4,13 +4,36 @@ Berkshire Hathaway Homestate Companies Interview Project
 ### Prompt
 Write an application that displays three or more reasons that you would like to work for BHHC.  Persist the list of reasons and use any means to display them.
 
+
+### Description
+My project is an informational web application for prospective new hires. The website includes information
+about the company and several benefits of being an employee of BHHC and current job listings that are
+specific to engineering fields.
+
+### How to Run
+1. Download the github repository on your local machine
+2. Install node JS https://nodejs.org/en/download/
+3. Install the following inside the repository:
+    npm install express
+    npm install cheerio
+    npm install request-promise
+4. Run the following command within the repository:
+    node app.js
+5. Go to `localhost:3000`
+6. Browse at your leisure
+
 ### Technologies
 - Node JS
 - Express
 - Bootstrap
 - HTML / CSS
 - JQuery
-- Cheerio 
+- Cheerio
+
+### Installations Needed
+- express
+- cheerio
+- request-promise
 
 ### Credits
 Icons made by Freepik from www.flaticon.com
--- app.js
+++ app.js
@@ -6,18 +6,28 @@ const $ = require('cheerio');
 const path = require('path');
 const url = 'https://www.bhhc.com/careers/current-openings.aspx?state=CA&jobarea=IT';
 
+//specify EJS template for html files
 app.engine('html', require('ejs').renderFile);
+//specify where template files are located
 app.set('views', path.join(__dirname, 'views'));
+//specify which template engine to use
 app.set('view engine', 'html');
+//serve files inside the public directory so they are accessible to index.html
 app.use(express.static('public'));
 
+/* -------------- parseArrays() Function --------------
+ This function will group together the job data for each
+ listing and format the data arrays into JSON data and
+ write to a JSON file. -------------------------------*/
 function parseArrays(titles, locations, websites){
   var jsonString = [];
+  //create a object that contains the job data for each job listing
   for(i = 0; i < titles.length; i++){
     var listing = { title : titles[i], location: locations[i], website: websites[i] }
+    //add object to an array
     jsonString.push(listing);
   }
-
+  //write string of job listings to a file after converting it to JSON format
   fs.writeFile('public/listings.json', JSON.stringify(jsonString, null, 4), function(err) {
     if(err) {
       throw err;
@@ -25,38 +35,47 @@ function parseArrays(titles, locations, websites){
   });
 }
 
+/* -------------- getListings() Function --------------
+ This function will scrape the BHHC job listings site
+ for the job data that we want and call parseArrays to
+ format the collected data. -------------------------*/
 function getListings() {
+  //initialize arrays to hold specific job data
   var titles = [];
   var locations = [];
   var websites = [];
-  requestPromise(url)
-    .then(function(html){
-      $('#CareerResults', html).each(function(i,element){
-        $('.title', html).each(function(i,element){
-          titles.push($(this).text());
-        });
-        $('.location', html).each(function(i,element){
-          locations.push($(this).text());
-        });
-        $('.website > p > a', html).each(function(i, element){
-          websites.push($(this).attr('href'));
-        });
+  //HTTP request to crawl the given url
+  requestPromise(url).then(function(html){
+    //for each job element of CareerResults div, collect data
+    $('#CareerResults', html).each(function(i,element){
+      $('.title', html).each(function(i,element){
+        titles.push($(this).text());
       });
-    })
-    .then(function(){
-      titles.splice(0,1);
-      locations.splice(0,1);
-      parseArrays(titles,locations, websites);
-    })
-    .catch(function(err){
-      console.log(err);
+      $('.location', html).each(function(i,element){
+        locations.push($(this).text());
+      });
+      $('.website > p > a', html).each(function(i, element){
+        websites.push($(this).attr('href'));
+      });
     });
+  }).then(function(){
+    //remove one element of each array starting at index 0
+    titles.splice(0,1);
+    locations.splice(0,1);
+    //pass job data to parseArrays
+    parseArrays(titles,locations, websites);
+  }).catch(function(err){
+    //if request fails, output the error
+    console.log(err);
+  });
 }
 
+//handles GET request from user so pass back the index page
 app.get('/', function (req, res) {
    res.render('index.html');
 });
 
+//when app.js is run, calls getListings() and listens for requests on port 3000
 app.listen(3000, function() {
   getListings();
   console.log("Application is running!")
